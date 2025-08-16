e2d03f729c156a883017f47aca9d2bca53899b88
Нечаева Наталья <miss.nechaewa@gmail.com>
2025-08-16 09:46:44 +0300

    add .

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -104,15 +105,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en"
 
 USE_I18N = True
 
-USE_TZ = True
-
-USE_L10N = True
+LANGUAGES = [
+    ('en', 'English'),
+    ('ru', 'Russian'),
+]
 
 LOCALE_PATHS = [
     BASE_DIR / "locale",
