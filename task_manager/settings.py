e9b1ee17d3e1e8e985d12becb47f4f02ad610c00
Нечаева Наталья <miss.nechaewa@gmail.com>
2025-08-26 23:36:14 +0300

    Fix static files settings for Render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -41,11 +41,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    "webserver",
-    "127.0.0.1",
-    "localhost",
-]
+ALLOWED_HOSTS = ["webserver", "localhost", "127.0.0.1", "python-project-52.onrender.com)"]
 
 
 # Application definition
