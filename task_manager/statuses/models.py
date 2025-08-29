3b1978baa6fb17a7cd41dc691d3a3257d04e1efb
Нечаева Наталья <miss.nechaewa@gmail.com>
2025-08-29 17:34:13 +0300

    add .

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.db import models
 
 class Status(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255 )
     created_at = models.DateTimeField(auto_now_add=True)  # для новых записей
 
     def __str__(self):
